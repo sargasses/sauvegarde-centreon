19b166d854de8170324ca04378f1946096aed520
sargasses <github@sargasses.fr>
2014-02-07 16:40:30 +0100

    Mise  jour fichier sauvegarde-centreon.sh

--- sauvegarde-centreon.sh
+++ sauvegarde-centreon.sh
@@ -2,7 +2,7 @@
 #
 # Copyright 2013-2014 
 # Développé par : Stéphane HACQUARD
-# Date : 04-02-2014
+# Date : 07-02-2014
 # Version 1.0
 # Pour plus de renseignements : stephane.hacquard@sargasses.fr
 
